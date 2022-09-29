8b16318fcccdd541a48844203ae551e149b44ace
harshita <harshita1225@gmail.com>
2022-09-29 20:53:02 +0200

    task completed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,25 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+import { Modal } from "bootstrap";
+import "../styles/main.scss";
 // Import any additional modules you want to include below \/
-
 
 // \/ All of your javascript should go here \/
 
+const modal = document.getElementById("myModal");
+const closeModal = document.querySelector(".close");
+
+const promiseOfModal = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve();
+  }, 6000);
+});
+
+promiseOfModal.then(() => {
+  modal.style.display = "block";
+});
+
+//close modal
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
